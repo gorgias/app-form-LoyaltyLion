eb85e47f0e2c6fd05ebf36c1a11c80ca67e5b958
UrosLombauer <81155055+UrosLombauer@users.noreply.github.com>
2021-07-07 18:12:34 +0200

    Update index.js

--- index.js
+++ index.js
@@ -3,45 +3,46 @@ document.addEventListener("DOMContentLoaded",function(){
   
     const integrationMethod = async(domain) => {
       const url = `https://${domain}.gorgias.com/api/integrations`; // form - gorgiasDomain
-      const options = {
-          method: 'POST',
-          mode: no-cors,
-          headers: {Accept: 'application/json', 'Content-Type': 'application/json'},
-          body: JSON.stringify({
+    //   const options = {
+    //       method: 'POST',
+             mode: no-cors,
+    //       headers: {Accept: 'application/json', 'Content-Type': 'application/json'},
+    //       body: JSON.stringify({
+    //         http: {
+    //           method: 'GET',
+    //           request_content_type: 'application/json',
+    //           response_content_type: 'application/json',
+    //           triggers: {
+    //             'ticket-created': true,
+    //             'ticket-updated': false,
+    //             'ticket-message-created': true
+    //           }
+    //         }
+    //       })
+    //     };
+
+        let options2 = {
+            type: "http",
+            name: "LoyaltyLion",
+            description: "Uros test",
             http: {
-              method: 'GET',
-              request_content_type: 'application/json',
-              response_content_type: 'application/json',
-              triggers: {
+                headers: {
+                Authorization: "Basic NzUxODViZWQwMDc5N2E3NjgzMTExMDcxM2Q5YWU1OWU6ZTc5ODJiNjRmODE2OWU5NGI1YTU1ZDg2NzNhOWMxODM="
+                },
+                url: "https://api.loyaltylion.com/v2/customers?email={{ticket.requester.email}}",
+                method: "GET",
+                request_content_type: "application/json",
+                response_content_type: "application/json",
+                triggers: {
                 'ticket-created': true,
                 'ticket-updated': false,
                 'ticket-message-created': true
-              }
+                },
+                "form": ""
             }
-          })
-        };
-
-//         let options2 = {
-//             type: "http",
-//             name: "LoyaltyLion",
-//             description: "Uros test",
-//             http: {
-//                 headers: {
-//                 },
-//                 url: "https://api.loyaltylion.com/v2/customers?email={{ticket.requester.email}}",
-//                 method: "GET",
-//                 request_content_type: "application/json",
-//                 response_content_type: "application/json",
-//                 triggers: {
-//                 'ticket-created': true,
-//                 'ticket-updated': false,
-//                 'ticket-message-created': true
-//                 },
-//                 "form": ""
-//             }
-//         }              
+        }              
   
-        fetch(url, options)
+        fetch(url, options2)
         .then(res => res.json())
         .then(json => {console.log('integration: ', json); return json;})
         .catch(err => {console.error('error:' + err); return err;});
